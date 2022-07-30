c01be4a8d7e7e93c2293ccbe3e0479199cd708ee
joshdev <joshuaajayi012@gmail.com>
2022-07-30 15:12:45 +0100

    calculator

---
+++ script.js
@@ -0,0 +1,14 @@
+// let display = document.getElementById('result')
+
+
+
+// function user(input){
+//     display.value += input;
+// }
+// function getclear(){
+//     document.getElementById('result').value = ''
+// }
+
+// function calResult(){
+//     display.value = eval(display.value);
+// }
